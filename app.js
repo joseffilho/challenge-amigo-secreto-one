1b2ef171db12c37d7b694bd88e5a4f43e911a058
filho <filhoserginho63@gmail.com>
2025-01-26 15:33:05 -0300

    criando array e funcao para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value;
+    if (nome === '' || nome.trim() === '') {
+        alert('Por favor, insira um nome.');
+    }
+    amigos.push(nome);
+    document.getElementById('amigo').value = '';
+    console.log(amigos);
+}
